bff38e18263b621bb7a5dad7f30c22c3e07d254b
Eugen Podbilschi <podbilschi@gmail.com>
2020-02-20 10:30:45 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
            <a class="navbar-brand" href="#">Project name</a>
         </div>
         <div class="links">
-          <a href="https://laravel.com/docs">Docs</a>
+          <a href="https://laravel.com/docs">MAMSMDASD</a>
           <a href="https://laracasts.com">Laracasts</a>
           <a href="https://laravel-news.com">News</a>
           <a href="https://blog.laravel.com">Blog</a>
